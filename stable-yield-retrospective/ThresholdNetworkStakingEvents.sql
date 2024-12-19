9f4d95ac76502903c3526700e190199a4a4280f7
Manuel Montenegro <manuel@nucypher.com>
2024-12-19 20:32:39 +0100

    Update Staking Events CSV format

--- stable-yield-retrospective/ThresholdNetworkStakingEvents.sql
+++ stable-yield-retrospective/ThresholdNetworkStakingEvents.sql
@@ -1,28 +1,28 @@
 WITH
     staked_events as (
         SELECT
-            to_unixtime(evt_block_time) as timestamp,
+            evt_block_time as timestamp,
             stakingProvider as staking_provider,
             'staked' as event,
-            amount/1e18 as amount
+            amount
         FROM
             threshold_network_ethereum.TokenStaking_evt_Staked
     ),
     topped_up_events as (
         SELECT
-            to_unixtime(evt_block_time) as timestamp,
+            evt_block_time as timestamp,
             stakingProvider as staking_provider,
             'toppedUp' as event,
-            amount/1e18 as amount
+            amount
         FROM
             threshold_network_ethereum.TokenStaking_evt_ToppedUp
     ),
     unstaked_events as (
         SELECT
-            to_unixtime(evt_block_time),
+            evt_block_time,
             stakingProvider as staking_provider,
             'unstaked' as event,
-            amount/1e18 as amount
+            amount
         FROM
             threshold_network_ethereum.TokenStaking_evt_Unstaked
     )
